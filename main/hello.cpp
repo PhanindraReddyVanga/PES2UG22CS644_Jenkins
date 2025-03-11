2015061497e094b956bc7206abac9717b1e4dae0
PhanindraReddyVanga <phanindrareddy1507@gmail.com>
2025-03-11 22:26:13 +0530

    hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "PES2UG22CS644\n";
-  cout << "Phanindra Reddy\n";
+  cout << "VKS PhanindraReddy\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
