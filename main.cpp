d560e5923178634887305b61f3ca1a69cd3fe649
PhanindraReddyVanga <phanindrareddy1507@gmail.com>
2025-03-11 22:40:31 +0530

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES2UG22CS644\n";
+  cout << "VKS Phanindra Reddy\n";
+  cout << "I have successfully built and run this  \n";
+  return 0;
+}
